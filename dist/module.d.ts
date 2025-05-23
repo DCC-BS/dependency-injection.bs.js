53493493318f8ac736843657f29faf8a84578710
GitHub Action <action@github.com>
2025-05-23 14:24:52 +0000

    Release v0.0.4

---
+++ dist/module.d.ts
@@ -0,0 +1,41 @@
+import * as _nuxt_schema from '@nuxt/schema';
+
+interface ServiceType<T> {
+    new (...args: any[]): T;
+    $injectKey: string;
+}
+interface IServiceProvider {
+    resolve<T>(target: ServiceType<T>): T;
+    resolveNamed<T>(key: string): T;
+}
+
+/**
+ * Class for building and validating service dependency graphs
+ * Handles registration of services and their dependencies
+ */
+
+interface IServiceProviderBuilder {
+    /**
+     * Register an instance of a service with no dependencies
+     */
+    registerInstance<T>(target: ServiceType<T>, instance: T): void;
+    registerNamedInstance<T>(name: string, instance: T): void;
+    /**
+     * Register a service type that will be instantiated later during building
+     */
+    register<T>(target: ServiceType<T>): void;
+    build(): IServiceProvider;
+}
+
+type ServiceProviderFactory = (builder: IServiceProviderBuilder) => void;
+declare class ServiceOrchestrator {
+    private builderFactory;
+    private serviceProvider;
+    setup(builderFactory: ServiceProviderFactory): void;
+    getProvider(): IServiceProvider;
+}
+
+declare const module: _nuxt_schema.NuxtModule<_nuxt_schema.ModuleOptions, _nuxt_schema.ModuleOptions, false>;
+
+export { ServiceOrchestrator, module as default };
+export type { IServiceProvider, IServiceProviderBuilder };
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-// export { ServiceOrchestrator } from "./service_orchestrator";
-// export type { IServiceProvider, ServiceType } from "./service_provider";
-// export type { IServiceProviderBuilder } from "./service_provider_builder";
