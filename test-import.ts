11e4d5c97bf6c45a22cc26df9fff1203db13101e
Tobias Bollinger <tobias.bollinger@gmail.com>
2025-05-23 17:27:27 +0200

    refactor: remove test file for ServiceOrchestrator

--- test-import.ts
+++
@@ -1,6 +0,0 @@
-// Test importing ServiceOrchestrator
-import { ServiceOrchestrator } from "./dist/module";
-
-// Try to use it as a value
-const orchestrator = new ServiceOrchestrator();
-console.log(orchestrator);
