6d84e9786f1aa117a593a8e19d8a573350ecc87c
GitHub Action <action@github.com>
2025-05-23 15:16:28 +0000

    Release v0.0.5

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { ServiceOrchestrator } from "./services/service_orchestrator";
-export type { IServiceProvider, ServiceType } from "./services/service_provider";
-export type { IServiceProviderBuilder } from "./services/service_provider_builder";
